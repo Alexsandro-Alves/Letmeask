5001b2333e539023aa54d1544d37b37b28de4a21
Alexsandro-Alves <alexsandro.ap.alves15@gmail.com>
2021-06-22 21:04:20 -0300

    criado os contextos de autenticação com o firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Button } from './components/Buttons'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
   return (
-      <Button text="Botão 1"/>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+        </AuthContextProvider>
+      </BrowserRouter>
   );
 }
 
